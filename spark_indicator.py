cf23b111ebccefabcd59ad0961a001c259aecd4b
HappyConcepts <happyconcepts.com@gmail.com>
2018-07-30 10:39:39 -0700

    desktop tweaks

--- spark_indicator.py
+++ spark_indicator.py
@@ -5,8 +5,7 @@
 # https://github.com/happyconcepts/spark-indicator
 # THIS SOFTWARE IS LICENSED FOR NON-COMMERCIAL USE ONLY!
 # use is subject to creative commons non-commercial share-alike 4.0 license
-# This open source software is released to improves transparency and security
-# for users of spark token
+
 VERSION = '1.1b'
 APPID 	= 'spark-indicator'
 
